5d09e83a1c1caa8b945cdc2174d17010056ddfb4
Henz <henz.jb@gmail.com>
2019-08-24 13:22:37 +0900

    finish get job info from redis - worker

--- worker/worker.py
+++ worker/worker.py
@@ -1,9 +1,10 @@
 import os
-from pika import BlockingConnection, ConnectionParameters
+from pika import BlockingConnection, ConnectionParameters, BasicProperties
 from logging import Logger
 from redis import Redis
-from typing import Union
-
+from typing import Union, List
+from constants import FILE_PATH_REDIS_KEY, JOB_STATUS_REDIS_KEY
+from job_status_enum import JobStatusEnum
 
 class Worker:
     def __init__(self, config: dict, logger: Logger):
@@ -18,10 +19,10 @@ class Worker:
         :return: current or new Redis client instance
         """
         if self.redisClient is not None:
-            self.logger.info("--------------- getting existing redis client -------------")
+            self.logger.info("------------------------ getting existing redis client --------------------")
             return self.redisClient
         try:
-            self.logger.info("--------------- creating new redis client -------------")
+            self.logger.info("------------------------ creating new redis client -----------------------")
             self.redisClient: Redis = Redis(host=self.config["kvs"]["host"], port=self.config["kvs"]["port"])
         except Exception as exc:
             self.logger.critical(exc)
@@ -39,25 +40,48 @@ class Worker:
         parameters = ConnectionParameters(host=self.config["queue"]["host"],
                                           port=self.config["queue"]["port"])
         try:
-            self.logger.info("---------------- creating new RabbitMQ connection -------------------")
+            self.logger.info("---------------- creating new RabbitMQ connection -----------------------")
             self.queueConn: BlockingConnection = BlockingConnection(parameters)
         except Exception as exc:
             self.logger.critical(exc)
             exit(1)
         return self.queueConn
 
-    def _onMessage(self, channel, method_frame, header_frame, body):
+    def getJobInfoFromRedis(self, jobId: str) -> List:
+        """
+        Get job information from Redis
+        :param jobId: id of the job
+        :return:
+        """
+        try:
+            self.logger.info("--------------------- getting data from redis -----------------------")
+            jobInfo: List = self.getRedisClient().hmget(jobId, [JOB_STATUS_REDIS_KEY, FILE_PATH_REDIS_KEY])
+            self.logger.info("------------------------ data from redis: %s ------------------------" % jobInfo)
+        except Exception as exc:
+            self.logger.critical(exc)
+            exit(1)
+        return jobInfo
+
+    def makeThumbnail(self, channel, method_frame, header_frame: BasicProperties, body: bytes):
         """
         Callback when receiving a message
+        This is the main logic of the worker
         :param channel: channel from which the message comes
         :param method_frame: method frame of the message
         :param header_frame: header frame of the message
         :param body: body of the message
         """
         jobId: str = body.decode("utf-8")
-        self.logger.info("---------------- receiving job: %s ---------------" % jobId)
+        self.logger.info("------------------------- receiving job: %s -----------------------" % jobId)
 
-        # Update job status in redis
+        # Get data from redis
+        jobInfo: List = self.getJobInfoFromRedis(jobId)
+
+        # Update job status in redis to ongoing
+
+        # Use ImageMagick to make thumbnail
+
+        # Update Job status in redis to finished
 
         # acknowledge message if treatment is finished
         channel.basic_ack(delivery_tag=method_frame.delivery_tag)
@@ -71,12 +95,11 @@ class Worker:
             channel = self.getQueueConnection().channel()
             queueName: str = self.config["queue"]["queueName"]
             channel.queue_declare(queueName, durable=True)
-            channel.basic_consume(queueName, self._onMessage)
+            channel.basic_consume(queueName, self.makeThumbnail)
             self.logger.info("--------------------- start_consuming -----------------------")
             channel.start_consuming()
         except Exception as exc:
             self.logger.critical(exc)
         finally:
-            channel.stop_consuming()
             self.queueConn.close()
             exit(1)
