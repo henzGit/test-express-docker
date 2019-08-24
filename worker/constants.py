0627d4538cbf8f129a3f869654228e7a9e0a81a8
Henz <henz.jb@gmail.com>
2019-08-24 19:32:39 +0900

    add constant thumbnail path redis key for worker

--- worker/constants.py
+++ worker/constants.py
@@ -1,3 +1,4 @@
 JOB_STATUS_REDIS_KEY = "jobstatus"
 FILE_PATH_REDIS_KEY = "filepath"
+THUMBNAIL_PATH_REDIS_KEY = "thumbnailpath"
 ERROR_SAME_JOB_STATUS = "A problem occurred due to same status for current and next job. Aborting"
