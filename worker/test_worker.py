44eab6342ff9bcd43e553babbba0967c6489c22c
Henz <henz.jb@gmail.com>
2019-08-24 11:53:51 +0900

    add test constructor

--- worker/test_worker.py
+++ worker/test_worker.py
@@ -1,10 +1,21 @@
 import unittest
-
+from worker import Worker
+from helper import setupLogging
+from logging import Logger
+from typing import Dict, Hashable, Any
 
 class TestWorker(unittest.TestCase):
-    def test_processJob(self):
-        self.assertEqual(True, False)
-
+    def test_constructor(self):
+        logger: Logger = setupLogging()
+        config: Dict[Hashable, Any] = {
+            'kvs': {'host': 'kvs', 'indexKey': 'redisIndexKey', 'port': 6379},
+            'queue': {'host': 'queue', 'port': 5672, 'queueName': 'test_queue'},
+        }
+        worker: Worker = Worker(config, logger)
+        self.assertEqual(worker.config, config)
+        self.assertEqual(worker.logger, logger)
+        self.assertEqual(worker.redisClient, None)
+        self.assertEqual(worker.queueConn, None)
 
 if __name__ == '__main__':
     unittest.main()
