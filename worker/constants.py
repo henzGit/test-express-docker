d27644efd93f1d6d52a4a9d619c6903988481026
Henz <henz.jb@gmail.com>
2019-08-24 20:26:19 +0900

    add constant thumbnail max pixel

--- worker/constants.py
+++ worker/constants.py
@@ -2,3 +2,4 @@ JOB_STATUS_REDIS_KEY = "jobstatus"
 FILE_PATH_REDIS_KEY = "filepath"
 THUMBNAIL_PATH_REDIS_KEY = "thumbnailpath"
 ERROR_SAME_JOB_STATUS = "A problem occurred due to same status for current and next job. Aborting"
+THUMBNAIL_MAX_PIXEL = 100
