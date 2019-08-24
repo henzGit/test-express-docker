edbb56589727d23f0df4545e37cb1b0b856a00c8
Henz <henz.jb@gmail.com>
2019-08-24 20:50:56 +0900

    add error processing image in worker constants

--- worker/constants.py
+++ worker/constants.py
@@ -1,5 +1,8 @@
 JOB_STATUS_REDIS_KEY = "jobstatus"
 FILE_PATH_REDIS_KEY = "filepath"
 THUMBNAIL_PATH_REDIS_KEY = "thumbnailpath"
+
 ERROR_SAME_JOB_STATUS = "A problem occurred due to same status for current and next job. Aborting"
+ERROR_PROCESSING_IMAGE = "A problem occurred during processing of image file with Image Magick."
+
 THUMBNAIL_MAX_PIXEL = 100
