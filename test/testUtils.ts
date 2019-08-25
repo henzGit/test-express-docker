c9d33ada5842b8abd548f9ec24f9279e44285ca1
Henz <henz.jb@gmail.com>
2019-08-25 16:18:53 +0900

    add mock thumbnail image file

--- test/testUtils.ts
+++ test/testUtils.ts
@@ -1,8 +1,10 @@
 import * as mockFs from "mock-fs";
 const IORedisMock = require("ioredis-mock");
+import { THUMBNAIL_PATH } from "./testConstants";
 
 export function createMockFs(): void {
   mockFs({
+    '/img/thumbnail/test.png': Buffer.from([8, 6, 7, 5, 3, 0, 9]),
     'test.png': Buffer.from([8, 6, 7, 5, 3, 0, 9]),
     'testDir': {}
   });
